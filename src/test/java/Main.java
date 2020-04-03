1f12f2ac3722e71e50142fdebfc39c8330fc7c60
rj <rawlinsicloud@gmail.com>
2020-04-03 10:31:33 -0700

    Merge branch 'master' of C:\Users\rj\IdeaProjects\ChallongeAPI with conflicts.

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -9,20 +9,18 @@ public class Main {
     static DecimalFormat mf = new DecimalFormat("#####.##");
 
     public static void main(String[] args) {
-        Challonge challonge = new Challonge("*****************", "NightShadePvP", mf.format(System.currentTimeMillis() * 2) + "", "IAMRJ's Tournament", "fun 1v1 tournament", GameType.SINGLE);
+        Challonge challonge = new Challonge("yd6TspZDs5dgXOnIeIptQgDKxPMKnsT7trYpFfVT", "IAMRJ", mf.format(System.currentTimeMillis() * 2) + "", "IAMRJ's Tournament", "fun 1v1 tournament", GameType.SINGLE);
 
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 39; i++) {
             challonge.getParticipants().add("test" + i);
         }
+
 
         challonge.post();
         challonge.addParticpants();
         challonge.start();
         challonge.indexMatches();
         System.out.println(challonge.getMatch(1));
-        for (String matchParticipant : challonge.getMatchParticipants(1)) {
-            System.out.println(matchParticipant);
-        }
-        System.out.println(challonge.getUrl());
+
     }
 }
