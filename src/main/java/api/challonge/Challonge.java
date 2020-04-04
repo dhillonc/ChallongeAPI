0411e3e014b811b7d43e289201ae4377507266d9
rj <rawlinsicloud@gmail.com>
2020-04-03 22:04:36 -0700

    legacy support unirest 2.4

--- src/main/java/api/challonge/Challonge.java
+++ src/main/java/api/challonge/Challonge.java
@@ -1,19 +1,19 @@
 package api.challonge;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.annotations.JsonAdapter;
+import api.challonge.GameType;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
-import kong.unirest.json.JSONArray;
-import kong.unirest.json.JSONObject;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @Getter
 @Setter
@@ -25,8 +25,8 @@ public class Challonge {
     private GameType gameType;
     private HttpResponse<JsonNode> response;
 
-    private HashMap<String, Integer> partId;
-    private HashMap<Integer, String> matchIds;
+    public static HashMap<String, Integer> partId = new HashMap<>();
+    public static HashMap<Integer, String> matchIds = new HashMap<>();
 
     public Challonge(String api, String username, String url, String name, String description, GameType gameType) {
         this.api = api;
@@ -35,8 +35,6 @@ public class Challonge {
         this.name = name;
         this.description = description;
         this.gameType = gameType;
-        partId = new HashMap<>();
-        matchIds = new HashMap<>();
         participants = new ArrayList<>();
     }
 
@@ -59,7 +57,7 @@ public class Challonge {
      * initial method for posting the match
      * @return
      */
-    public String post() {
+  /*  public boolean post3() {
         HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments.json")
                 .header("accept", "application/json")
                 .field("api_key", api)
@@ -69,14 +67,31 @@ public class Challonge {
                 .field("tournament[description]", description)
                 .asJson();
         this.response = response;
-        return response.getBody().toPrettyString();
+        return response.getStatus() == 200;
+    }*/
+
+
+    public CompletableFuture<Boolean> post() {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments.json")
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .field("tournament[name]", name)
+                    .field("tournament[url]", url)
+                    .field("tournament[tournament_type]", gameType.getName())
+                    .field("tournament[description]", description)
+                    .asJson();
+            this.response = response;
+            return response.getStatus() == 200;
+
+        });
     }
 
     /**
      *
      * @return if successful
      */
-    public boolean addParticpants() {
+   /* public boolean addParticpants() {
         HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/participants/bulk_add.json".replace("{tournament}", url))
                 .header("accept", "application/json")
                 .field("api_key", api)
@@ -97,14 +112,44 @@ public class Challonge {
             }
         }
         return response.getStatus() == 200;
+    }*/
+
+
+    public CompletableFuture<Boolean> addParticpants() {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/participants/bulk_add.json".replace("{tournament}", url))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .field("participants[][name]", participants)
+                    .asJson();
+
+            JSONArray jsonObject = response.getBody().getArray();
+            for (int i = 0; i < jsonObject.length(); i++) {
+
+                JSONObject object = jsonObject.getJSONObject(i);
+                Iterator keys = object.keys();
+                while (keys.hasNext()) {
+                    Object key = keys.next();
+                    JSONObject value = object.getJSONObject((String) key);
+                    int id = value.getInt("id");
+                    String name = value.getString("name");
+                    partId.put(name, id);
+                    System.out.println(name + id);
+                }
+            }
+            this.response = response;
+            return response.getStatus() == 200;
+
+        });
     }
 
 
+
     /**
-    *      Adds matches to HashMap based of their match ID starting at 1
+     *      Adds matches to HashMap based of their match ID starting at 1
      * @return if successful
-    * */
-    public boolean indexMatches() {
+     * */
+    /*public boolean indexMatches() {
         HttpResponse<JsonNode> response = Unirest.get("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches.json".replace("{tournament}", url))
                 .header("accept", "application/json")
                 .queryString("api_key", api)
@@ -125,6 +170,32 @@ public class Challonge {
             m++;
         }
         return response.getStatus() == 200;
+    }*/
+
+
+    public CompletableFuture<Boolean> indexMatches() {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.get("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches.json".replace("{tournament}", url))
+                    .header("accept", "application/json")
+                    .queryString("api_key", api)
+                    .asJson();
+
+            int m = 1;
+            JSONArray jsonObject = response.getBody().getArray();
+            for (int i = 0; i < jsonObject.length(); i++) {
+
+                JSONObject object = jsonObject.getJSONObject(i);
+                Iterator keys = object.keys();
+                while (keys.hasNext()) {
+                    Object key = keys.next();
+                    JSONObject value = object.getJSONObject((String) key);
+                    Integer id =  value.getInt("id");
+                    this.matchIds.put(m, String.valueOf(id));
+                }
+                m++;
+            }
+            return response.getStatus() == 200;
+        });
     }
 
     /**
@@ -139,7 +210,7 @@ public class Challonge {
      * Sets the tournament as started
      * @return if successful
      */
-    public boolean start() {
+    /*public boolean start() {
         HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/start.json".replace("{tournament}", url))
                 .header("accept", "application/json")
                 .field("api_key", api)
@@ -149,38 +220,45 @@ public class Challonge {
         } else {
             return false;
         }
+    }*/
+
+
+    public CompletableFuture<Boolean> start() {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/start.json".replace("{tournament}", url))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .asJson();
+            return response.getStatus() == 200;
+        });
     }
 
     /**
      * ends the tournament and archives the match post
      * @return
      */
-    public boolean end() {
-        HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/finalize.json".replace("{tournament}", url))
-                .header("accept", "application/json")
-                .field("api_key", api)
-                .asJson();
-        if (response.getStatus() == 200) {
-            return true;
-        } else {
-            return false;
-        }
+    public CompletableFuture<Boolean> end() {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/finalize.json".replace("{tournament}", url))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .asJson();
+            return response.getStatus() == 200;
+        });
     }
 
     /**
      * Randomizes the seeds
      * @return if successful
      */
-    public boolean randomize() {
-        HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/participants/randomize.json".replace("{tournament}", url))
-                .header("accept", "application/json")
-                .field("api_key", api)
-                .asJson();
-        if (response.getStatus() == 200) {
-            return true;
-        } else {
-            return false;
-        }
+    public CompletableFuture<Boolean> randomize() {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/randomize.json".replace("{tournament}", url))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .asJson();
+            return response.getStatus() == 200;
+        });
     }
 
     /**
@@ -188,7 +266,7 @@ public class Challonge {
      * @param id match id
      * @return the jsonarray of a match
      */
-    public JSONArray getMatch(int id) {
+  /*  public JSONArray getMatch(int id) {
         HttpResponse<JsonNode> response = Unirest.get("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}.json".
                 replace("{tournament}", url)
                 .replace("{match_id}", matchIds.get(id)))
@@ -196,15 +274,22 @@ public class Challonge {
                 .queryString("api_key", api)
                 .asJson();
         return response.getBody().getArray();
+    }*/
+
+
+    public CompletableFuture<JSONArray> getMatch(int id) {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.get("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}.json".
+                    replace("{tournament}", url)
+                    .replace("{match_id}", matchIds.get(id)))
+                    .header("accept", "application/json")
+                    .queryString("api_key", api)
+                    .asJson();
+            return response.getBody().getArray();
+        });
     }
 
-    /**
-     *
-     * @param matchId match id
-     * @param name winner
-     * @return if successful
-     */
-    public boolean updateMatch(int matchId, String name) {
+   /* public boolean updateMatch(int matchId, String name) {
         HttpResponse<JsonNode> response = Unirest.put("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}.json".
                 replace("{tournament}", url)
                 .replace("{match_id}", matchIds.get(matchId)))
@@ -215,8 +300,38 @@ public class Challonge {
                 .asJson();
 
         return response.getStatus() == 200;
+    }*/
+
+    public CompletableFuture<Boolean> updateMatch(int id, String name) {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.put("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}.json".
+                    replace("{tournament}", url)
+                    .replace("{match_id}", matchIds.get(id)))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .field("match[scores_csv]", "1-0")
+                    .field("match[winner_id]", String.valueOf(partId.get(name)))
+                    .asJson();
+
+            return response.getStatus() == 200;
+        });
     }
-    public boolean updateMatch(int matchId, int winnerId) {
+
+    public CompletableFuture<Boolean> updateMatch(int id, int winneriD) {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.put("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}.json".
+                    replace("{tournament}", url)
+                    .replace("{match_id}", matchIds.get(id)))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .field("match[scores_csv]", "1-0")
+                    .field("match[winner_id]", String.valueOf(winneriD))
+                    .asJson();
+
+            return response.getStatus() == 200;
+        });
+    }
+  /*  public boolean updateMatch(int matchId, int winnerId) {
         HttpResponse<JsonNode> response = Unirest.put("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}.json".
                 replace("{tournament}", url)
                 .replace("{match_id}", matchIds.get(matchId)))
@@ -227,10 +342,36 @@ public class Challonge {
                 .asJson();
 
         return response.getStatus() == 200;
+    }*/
+
+    public CompletableFuture<Boolean> markAsUnderway(int matchId) {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}/mark_as_underway.json".
+                    replace("{tournament}", url)
+                    .replace("{match_id}", matchIds.get(matchId)))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .asJson();
+
+            return response.getStatus() == 200;
+        });
+    }
+
+    public CompletableFuture<Boolean> unMarkAsUnderway(int matchId) {
+        return supplyAsync(() -> {
+            HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}/unmark_as_underway.json".
+                    replace("{tournament}", url)
+                    .replace("{match_id}", matchIds.get(matchId)))
+                    .header("accept", "application/json")
+                    .field("api_key", api)
+                    .asJson();
+
+            return response.getStatus() == 200;
+        });
     }
 
 
-    public boolean markAsUnderway(int matchId){
+    /*public boolean markAsUnderway(int matchId){
         HttpResponse<JsonNode> response = Unirest.post("https://" + username + ":" + api + "@api.challonge.com/v1/tournaments/{tournament}/matches/{match_id}/mark_as_underway.json".
                 replace("{tournament}", url)
                 .replace("{match_id}", matchIds.get(matchId)))
@@ -250,7 +391,7 @@ public class Challonge {
 
         return response.getStatus() == 200;
     }
-
+*/
 
 
     /**
@@ -258,19 +399,37 @@ public class Challonge {
      * @param matchId
      * @return array of particpants in the match
      */
-    public Integer[] getMatchParticipants(int matchId) {
-        JSONObject match = getMatch(matchId).getJSONObject(0).getJSONObject("match");
+
+    public CompletableFuture<Integer[]> getMatchParticipants(int matchId) {
+        return supplyAsync(() -> {
+            JSONObject match = null;
+            try {
+                match = getMatch(matchId).get().getJSONObject(0).getJSONObject("match");
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            return new Integer[]{
+                    (match.getInt("player1_id")),
+                    match.getInt("player2_id")
+            };
+        });
+    }
+
+
+  /*  public Integer[] getMatchParticipants(int matchId) throws ExecutionException, InterruptedException {
+        JSONObject match = getMatch(matchId).get().getJSONObject(0).getJSONObject("match");
 
         return new Integer[]{
                 (match.getInt("player1_id")),
                 match.getInt("player2_id")
         };
-    }
+    }*/
 
-    public String getRound(int matchId) {
+/*    public String getRound(int matchId) {
         JSONObject match = getMatch(matchId).getJSONObject(0).getJSONObject("match");
         return (String) match.get("round");
-    }
+    }*/
 
   /* public ArrayList<JSONObject> getMatches(String participantName) {
         ArrayList<JSONObject> matches = new ArrayList<>();
